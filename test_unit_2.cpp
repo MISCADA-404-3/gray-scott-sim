3f61e8dd9189126cfe33358442477c866b9c2765
iinnnkkkk <1452914867@qq.com>
2024-11-13 21:33:27 +0000

    style:test_unit_2.cpp
    Change to unified variable names.

--- test_unit_2.cpp
+++ test_unit_2.cpp
@@ -2,11 +2,11 @@
 #include <vector>
 #include "gs.cpp"
 
-TEST(DimensionTest, UVSameDimensions) {
+TEST(gsTest, gs_size_consistency_test) {
     // Check if the number of rows in u and v are the same
-    EXPECT_EQ(u.size(), v.size()) << "u 和 v 的行数不相同";
+    EXPECT_EQ(u.size(), v.size()) << "The number of rows of u and v are not the same";
 
     // Check if the number of columns in the first row is the same 
     //(since all rows have the same number of columns when the vector is initialized)
-    EXPECT_EQ(u[0].size(), v[0].size()) << "u 和 v 的列数不相同";
+    EXPECT_EQ(u[0].size(), v[0].size()) << "The number of columns of u and v are not the same";
 }
